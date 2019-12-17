f1d510427c2a20b1442c15a3bb52656bd96f18ea
Maxsuel Storch <55251002+maxbarboz@users.noreply.github.com>
2019-12-17 16:32:22 -0300

    Add files via upload

---
+++ JavaScript/Aulas - Modulo 1 ao 14/README.md
@@ -0,0 +1,26 @@
+# JavaScriptNARUTO
+
+Estudo da linguagem JavaScript
+
+# Descrição
+
+Originalmente projetada para rodar no Netscape Navigator, ela tinha o propósito de oferecer aos desenvolvedores 
+formas de tornar determinados processos de páginas web mais dinâmicos, tornando seu uso mais agradável. 
+
+# MÓDULOS EM ANDAMENTO
+
+## [x] MÓDULO 1 -
+## [x] MÓDULO 2 -
+## [x] MÓDULO 3 - 
+## [x] MÓDULO 4 -
+## [x] MÓDULO 5 -
+## [x] MÓDULO 6 -
+## [x] MÓDULO 7 -
+## [x] MÓDULO 8 -
+## [x] MÓDULO 9 -
+## [x] MÓDULO 10 -
+## [x] MÓDULO 11 -
+## [ ] MÓDULO 12 -
+## [ ] MÓDULO 13 -
+## [ ] MÓDULO 14 -
+## [ ] MÓDULO 15 -
--- JavaScript/Aulas - Modulo 15 ao 25/jsinbrowser/js/main.js
+++ JavaScript/Aulas - Modulo 15 ao 25/jsinbrowser/js/main.js
@@ -1,5 +1,40 @@
 ( function(){
-	
-	console.log( " to de hack foda-se ")
+
+var object = {
+	propriedade: 1,
+	init: function init(){
+		// faz referência ao objeto
+		return  this.propriedade;
+	}
+}
+
+console.log( object.init() )
+
+// quando você imprime uma variável this fora de um objeto, ela aponta para um escopo global, oque retorna window
+	function md(){
+		return this;
+	}
+
+	console.log( md() )
+
+// criando construtor em JS
+
+	function MyConstructor(){
+		this.prop1 = "Maxsuel";
+		this.prop2 = "Storch";
+	}
+
+	var vari = new MyConstructor();
+
+	console.log ( vari )
+
+	// quando voce usa this sem um var, você coloca essa variável em escopo global
+
+	prop1 = "TESTE";
+	console.log( window.prop1 )
+
+	// SE VOCÊ CHAMA a função sem usar new você acaba usando this para pendurar a variável em escopo global
 
 })();
+
+
